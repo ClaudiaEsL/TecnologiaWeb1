b3f4892426b8f2d8534367c4dcc1d1893ee26676
ClaudiaEsL <abiC.CAEL@gmail.com>
2020-09-29 17:01:33 -0400

    commit

--- Avances del proyecto/Avance 3/Refugio animal/JS/ObtenerFechaHora.js
+++ Avances del proyecto/Avance 3/Refugio animal/JS/ObtenerFechaHora.js
@@ -1,10 +1,10 @@
-
+/*Obtener la fecha y hora actual */
 var fecha_actual = new Date();
-
+/*Fecha*/
 var day = fecha_actual.getDay();
 var month = fecha_actual.getMonth();
 var year = fecha_actual.getFullYear();
-
+/*Hora */
 var hora = fecha_actual.getHours();
 var minutos = fecha_actual.getMinutes();
 var segundos = fecha_actual.getSeconds();
